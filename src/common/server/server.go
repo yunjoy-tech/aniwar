2ef34fc0ffe1f7d11e184996008235651991ba1c
吴俊杰 <1132431868@qq.com>
2026-01-05 17:58:46 +0800

    优化service接口

--- src/common/server/server.go
+++ src/common/server/server.go
@@ -4,13 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"gitee.com/aniwar2/aniwar/src/common/actor/stub"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-
 	"gitee.com/aniwar2/aniwar/src/common"
+	"gitee.com/aniwar2/aniwar/src/common/actor/stub"
 	"gitee.com/aniwar2/aniwar/src/common/conf"
 	"gitee.com/aniwar2/aniwar/src/common/db"
 	"gitee.com/aniwar2/aniwar/src/common/sdkconstant"
@@ -24,12 +19,14 @@ import (
 	svc "gitee.com/aniwar2/musae/service"
 	"gitee.com/aniwar2/musae/state"
 	"gitee.com/aniwar2/musae/tcpx"
-	"gitee.com/aniwar2/musae/utils"
 	dapr "github.com/dapr/go-sdk/client"
-	daprCommon "github.com/dapr/go-sdk/service/common"
 	"github.com/xuri/excelize/v2"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // 一个通用的微服务实例结构，对创建微服务需要的能力进行封装
@@ -87,64 +84,6 @@ func (s *Server) ConvUAID(uaid string) (string, uint64) {
 	}
 	accountId := uaid[0:idx]
 	return accountId, roleId
-}
-
-// 启动流程3: Server
-// Server Start逻辑
-func (s *Server) Start() error {
-	defer func() {
-		if err := recover(); err != any(nil) {
-			logger.Fatal("[server] Start recover, err: ", err)
-		}
-	}()
-
-	// 启动流程4: 初始化service的http,网络等组件
-	if err := s.InitBase(); err != nil {
-		return err
-	}
-	logger.Info("[server] base init success")
-
-	if err := s.OnPreInit(); err != nil {
-		return err
-	}
-
-	if conf.Base().IsDebug {
-		s.RegisterBindingInvocationHandler("api/status", func(ctx context.Context, in *daprCommon.BindingEvent) (out []byte, err error) {
-			return []byte(s.Status()), nil
-		})
-
-		s.RegisterBindingInvocationHandler("api/excel", func(ctx context.Context, in *daprCommon.BindingEvent) (out []byte, err error) {
-			return GetExcelData(string(in.Data)), nil
-		})
-	}
-
-	logger.Info("[server] pre init success")
-
-	// 启动流程5: 核心逻辑, 启动dapr service和dapr client等
-	if err := s.InitCore(); err != nil {
-		return err
-	}
-
-	s.InitConfigCenter()
-
-	if err := s.OnPostInit(); err != nil {
-		return err
-	}
-	utils.GoSafeRunNoError(func() {
-		t := time.NewTicker(time.Second * time.Duration(conf.Base().ServerHeartbeatInterval))
-		defer t.Stop()
-		for {
-			select {
-			case <-t.C:
-				utils.SafeRunNoError(s.OnUpdateStatus)
-			}
-		}
-	})
-
-	szLog := fmt.Sprintf("server start success, appid:%s version:%s rolling:%s", global.AppID, global.APP_VERSION, global.ROLLING_VERSION)
-	logger.Info(szLog)
-	// todo 服务启动通知
-	return nil
 }
 
 func (s *Server) OnUpdateStatus() {
---
+++ src/common/server/server_start.go
@@ -0,0 +1,70 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"gitee.com/aniwar2/aniwar/src/common/conf"
+	"gitee.com/aniwar2/musae/global"
+	"gitee.com/aniwar2/musae/logger"
+	"gitee.com/aniwar2/musae/utils"
+	daprCommon "github.com/dapr/go-sdk/service/common"
+	"time"
+)
+
+// 启动流程3: Server
+// Server Start逻辑
+func (s *Server) Start() error {
+	defer func() {
+		if err := recover(); err != any(nil) {
+			logger.Fatal("[server] Start recover, err: ", err)
+		}
+	}()
+
+	// 启动流程4: 初始化service的http,网络等组件
+	if err := s.InitBase(); err != nil {
+		return err
+	}
+	logger.Info("[server] base init success")
+
+	if err := s.OnPreInit(); err != nil {
+		return err
+	}
+
+	if conf.Base().IsDebug {
+		s.RegisterBindingInvocationHandler("api/status", func(ctx context.Context, in *daprCommon.BindingEvent) (out []byte, err error) {
+			return []byte(s.Status()), nil
+		})
+
+		s.RegisterBindingInvocationHandler("api/excel", func(ctx context.Context, in *daprCommon.BindingEvent) (out []byte, err error) {
+			return GetExcelData(string(in.Data)), nil
+		})
+	}
+
+	logger.Info("[server] pre init success")
+
+	// 启动流程5: 核心逻辑, 启动dapr service和dapr client等
+	if err := s.InitCore(); err != nil {
+		return err
+	}
+
+	s.InitConfigCenter()
+
+	if err := s.OnPostInit(); err != nil {
+		return err
+	}
+	utils.GoSafeRunNoError(func() {
+		t := time.NewTicker(time.Second * time.Duration(conf.Base().ServerHeartbeatInterval))
+		defer t.Stop()
+		for {
+			select {
+			case <-t.C:
+				utils.SafeRunNoError(s.OnUpdateStatus)
+			}
+		}
+	})
+
+	szLog := fmt.Sprintf("server start success, appid:%s version:%s rolling:%s", global.AppID, global.APP_VERSION, global.ROLLING_VERSION)
+	logger.Info(szLog)
+	// todo 服务启动通知
+	return nil
+}
